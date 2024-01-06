2a696aa4403cba45aee4b8aa87a6897df955658c
Cristofer Treglia <40124145+CrisTreglia@users.noreply.github.com>
2024-01-05 22:35:52 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,6 +62,18 @@ function updateProfessionalExperience(profileData) {
     }).join('')
 }
 
+function updateEducation(profileData) {
+    const education = document.getElementById('profile.education')
+    education.innerHTML = profileData.education.map(education => {
+        return `
+            <li>
+                <h3 class="title">${education.name}</h3>
+                <p class="period">${education.period}</p>
+            </li>
+        `
+    }).join('')
+}
+
 (async () => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
@@ -70,4 +82,5 @@ function updateProfessionalExperience(profileData) {
     updateLanguages(profileData)
     updatePortfolio(profileData)
     updateProfessionalExperience(profileData)
+    updateEducation(profileData)
 })()
